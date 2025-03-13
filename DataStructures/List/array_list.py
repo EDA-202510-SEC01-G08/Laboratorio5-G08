b53c7177eb43bdf647d3a256be62b63d79d8c0db
juanccancelado <j.canceladod@uniandes.edu.co>
2025-03-13 17:47:54 -0500

    ajuste default e intento de merge fallido

--- DataStructures/List/array_list.py
+++ DataStructures/List/array_list.py
@@ -71,6 +71,8 @@ def sub_list(my_list, start, num_elements):
         for i in range(start, num_elements):
             add_last(sublist, my_list["elements"][i])
         return sublist
+    
+
 
 def remove_last(my_list):
     if my_list["size"] == 0:
@@ -97,10 +99,14 @@ def remove_first(my_list):
 #Laboraorio 5: Funciones de ordenamiento con array list
 
 def default_sort_criteria(element_1, element_2):
-    if element_1 < element_2:
-        return True
+    if element_1 > element_2:
+        return 1
+    
+    elif element_1 < element_2:
+        return 2
+    
     else:
-        return False
+        return 3
 
 def selection_sort(my_list, sort_criteria):
     tamano = size(my_list)
@@ -115,10 +121,11 @@ def selection_sort(my_list, sort_criteria):
             f = 1 + i
             pos_menor = i
             while f < tamano:
-                sort = sort_criteria(get_element(my_list, f),menor)
+                sort = sort_criteria(get_element(my_list, f),menor)# pedro edita esto con base al nuevo sort criteria
                 if sort == True:
                     menor = get_element(my_list, f)
                     pos_menor = f
+
                 f += 1
             exchange(my_list, i, pos_menor)
         
@@ -137,7 +144,7 @@ def insertion_sort(my_list, sort_crit):
             f = i - 1
             x = i
             while f > -1:
-                sort = sort_crit(get_element(my_list, x), get_element(my_list,f))
+                sort = sort_crit(get_element(my_list, x), get_element(my_list,f))# lo mismo pq la f me confunde xddd
                 if sort == True:
                     exchange(my_list, x, f)
                     x -= 1
@@ -156,7 +163,7 @@ def shell_sort(my_list, sort_crit):
 
     else:
 
-        gap = n // 2 #?????
+        gap = n // 2
 
         while gap > 0:
 
@@ -164,7 +171,7 @@ def shell_sort(my_list, sort_crit):
                 elem = get_element(my_list, i)
                 j = i
 
-                while j >= gap and sort_crit(elem, get_element(my_list, j - gap)):
+                while j >= gap and sort_crit(elem, get_element(my_list, j - gap)) == 2:
 
                     exchange(my_list, j, j-gap)
                     j -= gap
@@ -173,8 +180,82 @@ def shell_sort(my_list, sort_crit):
 
         result = my_list
 
-    return my_list
+    return result
 
 
 
-     
+
+def merge_sort(my_list, sort_crit):
+
+    if size(my_list) == 0 or size(my_list) == 1:
+        return my_list
+
+    n = size(my_list)
+    m = n // 2
+
+    sub1 = sub_list(my_list, 0, m)
+    sub1 = merge_sort(sub1, sort_crit)
+
+    sub2 = new_list()
+    if n % 2 != 0:
+        sub2 = sub_list(my_list, m, m+1)
+
+    else:
+        sub2 = sub_list(my_list, m, m)
+
+    sub2 = merge_sort(sub2, sort_crit)
+
+    return merge(sub1,sub2, sort_crit)
+
+def merge(lista1, lista2, sort_crit):
+    lista = new_list
+
+    i = 0 
+    j = 0 
+
+    while i < size(lista1) or j < size(lista2):
+
+        sort = sort_crit(get_element(lista1, i), get_element(lista2, j))
+
+        if sort == 1:
+            add_last(lista, get_element(lista2, j))
+          
+
+        elif sort == 2:
+            add_last(lista, get_element(lista1, i))
+            
+        else:
+            add_last(lista, get_element(lista1, i))
+  
+
+        
+
+    return lista
+
+
+
+
+def partition(arr, low, high):
+
+    pivot = get_element(arr,high)
+
+    i = low - 1
+
+    for j in range(low, high):
+        if get_element(arr,j) < pivot:
+            i += 1
+            exchange(arr, i, j)
+    
+    exchange(arr, i + 1, high)
+    return i + 1
+
+
+
+def quickSort(arr, low, high, sort_crit):
+    if sort_crit(low, high) == 2:
+        
+        pi = partition(arr, low, high)
+        
+        quickSort(arr, low, pi - 1,sort_crit)
+        quickSort(arr, pi + 1, high,sort_crit)
+
